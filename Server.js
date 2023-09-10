a4dcb9cfb0ca36546b550a4205250f5ba7a7dc8b
harsh020718 <124969798+harsh020718@users.noreply.github.com>
2023-09-10 20:06:14 +0530

    Update Server.js

--- Server.js
+++ Server.js
@@ -8,11 +8,11 @@ const app = express();
 const port = process.env.PORT || 5000;
 // const allowedOrigins = ['https://frontend-hazel-one-41.vercel.app'];
 
-app.use((req,res,next)=>{
-  res.header('Access-Control-Allow-Origin','*');
-  next();
-});
-app.use(cors({}));
+// app.use((req,res,next)=>{
+//   res.header('Access-Control-Allow-Origin','*');
+//   next();
+// });
+// app.use(cors({}));
 app.use(express.json());
 app.use("/api/auth", require("./Routes/auth"));
 app.use("/api/doingR", require("./Routes/doingR"));
