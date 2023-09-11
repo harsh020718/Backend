b1fbfd04254308235d4479fa0ad4803469d1e907
harsh020718 <124969798+harsh020718@users.noreply.github.com>
2023-09-11 16:18:30 +0530

    Update db.js

--- db.js
+++ db.js
@@ -1,17 +1,17 @@
-require('dotenv').config();
+
 const mongoose = require("mongoose");
-const mongoURI = process.env.DATABASE;
+const mongoURI = process.env.DATABASE
 // console.log(process.env.DATABASE);
 const connectToMongo = async () => {
     try {
-      console.log("F");
+      
       await mongoose.connect(mongoURI);
       console.log("Connected to Mongo Successfully");
   
       // Create a sample document in a collection to create the database
       
     } catch (error) {
-      console.log("FS");
+      
       console.error("Error connecting to Mongo:", error);
     }
   };
